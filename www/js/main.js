971beaa9bdb2c620fca94438fb9515432573d6b9
Amir Jann Castaneda <amirj.castaneda@gmail.com>
2022-01-09 05:31:25 +0800

    Added reset functionality

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,7 @@
 const timer = {
-	pomodoro: 25,
-	shortBreak: 5,
-	longBreak: 15,
-	longBreakInterval: 4,
+	pomodoro: .2,
+	shortBreak: .1,
+	longBreak: .15,
 	sessions: 0
 };
 
@@ -11,23 +10,41 @@ let interval;
 controlButton = document.querySelector("#controlButton");
 controlButton.addEventListener("click", () => {
 	const control = controlButton.dataset.control;
-	if (control == 'start') 
-		startTimer();
-	else if (control == 'stop') 
-		stopTimer();
+	console.log(control);
+	switch (control) {
+		case 'start':
+			startTimer();
+			break;
+		case'stop':
+			stopTimer();
+			break;
+	}
 });
+document.querySelector("#resetButton").addEventListener('click', resetTimer);
 document.querySelector("#mode-buttons").addEventListener('click', handleMode);
 
 function startTimer()  {
 	controlButton.dataset.control  = 'stop';
 	controlButton.textContent = 'stop';
+
 	interval = setInterval(function() {
 		timer.remainingTime -= 1;
 		updateClock();
+		console.log(timer.remainingTime);
 
 		remainingTime = timer.remainingTime;
-		console.log(remainingTime, typeof remainingTime);
-		if (remainingTime <= 0) {
+		if (remainingTime < 0) {
+			clearInterval(interval);
+			if (timer.mode === 'pomodoro') {
+				timer.sessions++;
+				if (timer.sessions % 4 == 0)
+					switchMode('longBreak');
+				else
+					switchMode('shortBreak');
+			}
+			else {
+				switchMode('pomodoro');
+			}
 			stopTimer();
 		}
 	}, 1000); 
@@ -37,6 +54,14 @@ function stopTimer() {
 	controlButton.dataset.control  = 'start';
 	controlButton.textContent = 'start';
 	clearInterval(interval);
+}
+
+function resetTimer() {
+	if (controlButton.dataset.control === 'stop') {
+		stopTimer();
+		switchMode(timer.mode);
+	}
+		return;
 }
 
 function updateClock() {
@@ -50,6 +75,11 @@ function updateClock() {
 function switchMode(mode) {
 	timer.mode = mode;
 	timer.remainingTime = timer[mode] * 60;
+
+	const currentMode = document.getElementsByClassName("active");
+	currentMode[0].className = currentMode[0].className.replace(" active", "")
+	document.querySelector(`#${mode}`).classList.add("active");
+
 	updateClock();
 }
 
@@ -57,9 +87,6 @@ function switchMode(mode) {
 function handleMode(event) {
 	const mode = event.target.id;
 	if (mode === "mode-buttons") return;
-	const currentMode = document.getElementsByClassName("active");
-	currentMode[0].className = currentMode[0].className.replace(" active", "")
-	event.target.classList.add("active");
 
 	switchMode(mode);
 	stopTimer();
