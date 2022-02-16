b67270f89d6a48a535e1951ca99743f4956202e3
Amir <amirj.castaneda@gmail.com>
2022-02-17 04:31:48 +0800

    Initial server, re-structured the directories

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// Dependencies and Modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+
+// Server & MongoDB setup
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+const mongodbConnectionString = process.env.MONGODB_CONNECTION_STRING;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.connect(mongodbConnectionString, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "Connection Error with MongoDB"));
+db.once('open', () => console.log('successfully connected to MongoDB.'));
+
+app.use('/users', userRoutes);
+app.listen(port, () => console.log(`Successfully connected to port ${port}`));
---
+++ server/models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+	task: {
+		type: String, 
+    	unique: true, 
+    	required: true
+  	},
+   description: {
+    	type: String,  
+  },
+  completed: {
+    	type: Boolean, 
+    	default: false
+  	}
+});
+
+module.exports = mongoose.model('Task', taskSchema);
